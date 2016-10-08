1cc9d21d4888922bb8fe789a038295e24cead4a6
Orangemirage <theartsconnection@Thes-iMac-6.local>
2016-10-07 17:17:40 -0700

    start
    adding XcoDe

---
+++ [ Insert Name Here ]/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  [ Insert Name Here ]
+//
+//  Created by The Arts Connection 1 on 2016-09-30.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ [ Insert Name Here ]/[ insert playgound here ].playground/Contents.swift
@@ -0,0 +1,3 @@
+import UIKit
+import PlaygroundSupport
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
